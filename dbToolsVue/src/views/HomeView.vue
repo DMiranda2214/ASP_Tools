42c44e28da69059be47d1aaa459f4a307fae626c
David Miranda <dm1236789@gmail.com>
2024-07-06 22:54:46 -0500

    Inicializacion conexion base de datos

--- dbToolsVue/src/views/HomeView.vue
+++ dbToolsVue/src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container fluid class="fill-height">
+    <v-sheet class="mx-auto" width="600">
+      <h1 aling="center">Postgres Tools</h1>
+      <br>
+      <v-form fast-fail @submit.prevent>
+        <v-text-field label="URL Server"></v-text-field>
+        <v-text-field label="Port" type="number"></v-text-field>
+        <v-text-field label="DataBase"></v-text-field>
+        <v-text-field label="Username"></v-text-field>
+        <v-text-field label="Password" type="password"></v-text-field>
+        <v-btn width="200" class="mt-4" type="submit" block>Ingresar</v-btn>
+      </v-form>
+    </v-sheet>
+  </v-container>
 </template>
+<script setup>
+
+
+</script>
